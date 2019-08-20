40313a47ebd1360f3452bbb7f115fe65f3b8ca49
Jean Flaherty <kobejean@me.com>
2019-08-19 21:53:11 -0600

    Custom op setup

--- Python/moji/python/ops/zero_out_ops.py
+++ Python/moji/python/ops/zero_out_ops.py
@@ -1,19 +1,26 @@
-# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2018 The Sonnet Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
-"""TensorFlow custom op example."""
+# ============================================================================
+"""Use zero_out ops in python."""
 
 from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
-from moji.python.ops.zero_out_ops import zero_out
+from tensorflow.python.framework import load_library
+from tensorflow.python.platform import resource_loader
+
+zero_out_ops = load_library.load_op_library(
+    resource_loader.get_path_to_datafile('_zero_out_ops.so'))
+zero_out = zero_out_ops.zero_out
